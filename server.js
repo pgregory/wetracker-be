993cbd4e21a8b8c7d30c8912db388723e334bc85
Paul Gregory <aqsis1@gmail.com>
2017-01-14 15:06:03 +0000

    Complete placeholder API code

--- server.js
+++ server.js
@@ -39,6 +39,13 @@ function handleError(res, reason, message, code) {
  */
 
 app.get("/contacts", function(req, res) {
+  db.collection(CONTACTS_COLLECTION).find({}).toArray(function(err, docs) {
+    if (err) {
+      handleError(res, err.message, "Failed to get contacts.");
+    } else {
+      res.status(200).json(docs);
+    }
+  });
 });
 
 app.post("/contacts", function(req, res) {
@@ -65,11 +72,35 @@ app.post("/contacts", function(req, res) {
  */
 
 app.get("/contacts/:id", function(req, res) {
+  db.collection(CONTACTS_COLLECTION).findOne({ _id: new ObjectID(req.params.id) }, function (err, doc) {
+    if (err) {
+      handleError(res, err.message, "Failed to get contact.");
+    } else {
+      res.status(200).json(doc);
+    }
+  });
 });
 
 app.put("/contacts/:id", function(req, res) {
+  var updateDoc = req.body;
+  delete updateDoc._id;
+
+  db.collection(CONTACTS_COLLECTION).updateOne({ _id: new ObjectID(req.params.id) }, updateDoc, function (err, doc) {
+    if (err) {
+      handleError(res, err.message, "Failed to update contact.");
+    } else {
+      res.status(204).json(doc);
+    }
+  });
 });
 
 app.delete("/contacts/:id", function(req, res) {
+  db.collection(CONTACTS_COLLECTION).deleteOne({ _id: new ObjectID(req.params.id) }, function (err, result) {
+    if (err) {
+      handleError(res, err.message, "Failed to delete contact.");
+    } else {
+      res.status(204).end();
+    }
+  });
 });
 
