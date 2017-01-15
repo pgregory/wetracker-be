47363bcd1ca1bde4eef31f8e84279eac1e52f679
Paul Gregory <aqsis1@gmail.com>
2017-01-15 19:30:21 +0000

    Refactor to serve demo songs database.

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ var bodyParser = require('body-parser');
 var mongodb = require('mongodb');
 var ObjectID = mongodb.ObjectID;
 
-var CONTACTS_COLLECTION = "contacts";
+var SONGS_COLLECTION = "songs";
 
 var app = express();
 app.use(express.static(__dirname + "/public"));
@@ -12,7 +12,9 @@ app.use(bodyParser.json());
 
 var db;
 
-mongodb.MongoClient.connect(process.env.MONGODB_URI, function (err, database) {
+var uri = process.env.MONGODB_URI || "mongodb://localhost:27017/wetracker";
+
+mongodb.MongoClient.connect(uri, function (err, database) {
   if (err) {
     console.log(err);
     process.exit(1);
@@ -32,74 +34,97 @@ function handleError(res, reason, message, code) {
   res.status(code || 500).json({"error": message});
 }
 
+app.all('*', function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Methods", 'PUT, GET, POST, DELETE, OPTION');
+  res.header("Access-Control-Allow-Headers", "Content-Type");
+  next();
+ });
 
-/* "/contacts"
- * GET: finds all contacts
- * POST: creates a new contact
+
+/* "/songs"
+ * GET: finds all songs
+ * POST: creates a new song
  */
 
-app.get("/contacts", function(req, res) {
-  db.collection(CONTACTS_COLLECTION).find({}).toArray(function(err, docs) {
+app.get("/songs", function(req, res) {
+  db.collection(SONGS_COLLECTION).find({}).toArray(function(err, docs) {
     if (err) {
-      handleError(res, err.message, "Failed to get contacts.");
+      handleError(res, err.message, "Failed to get songs.");
     } else {
       res.status(200).json(docs);
     }
   });
 });
 
-app.post("/contacts", function(req, res) {
-  var newContact = req.body;
-  newContact.createDate = new Date();
+app.post("/songs", function(req, res) {
+  var newSong = req.body;
+  newSong.createDate = new Date();
 
-  if (!(req.body.firstName || req.body.lastName)) {
-    handleError(res, "Invalid user input", "Must provide a first and last name", 400);
+  if (!(req.body.name)) {
+    handleError(res, "Invalid user input", "Must provide a song name", 400);
   }
 
-  db.collection(CONTACTS_COLLECTION).insertOne(newContact, function(err, doc) {
+  db.collection(SONGS_COLLECTION).insertOne(newSong, function(err, doc) {
     if (err) {
-      handleError(res, err.message, "Failed to create new contact.");
+      handleError(res, err.message, "Failed to create new song.");
     } else {
       res.status(201).json(doc.ops[0]);
     }
   });
 });
 
-/* "/contacts/:id"
- * GET: find contact by id
- * PUT: update contact by id
- * DELETE: deletes contact by id
+/* "/songs/:id"
+ * GET: find song by id
+ * PUT: update song by id
+ * DELETE: deletes song by id
  */
 
-app.get("/contacts/:id", function(req, res) {
-  db.collection(CONTACTS_COLLECTION).findOne({ _id: new ObjectID(req.params.id) }, function (err, doc) {
+app.get("/songs/:id", function(req, res) {
+  db.collection(SONGS_COLLECTION).findOne({ _id: new ObjectID(req.params.id) }, function (err, doc) {
     if (err) {
-      handleError(res, err.message, "Failed to get contact.");
+      handleError(res, err.message, "Failed to get song.");
     } else {
       res.status(200).json(doc);
     }
   });
 });
 
-app.put("/contacts/:id", function(req, res) {
+app.put("/songs/:id", function(req, res) {
   var updateDoc = req.body;
   delete updateDoc._id;
 
-  db.collection(CONTACTS_COLLECTION).updateOne({ _id: new ObjectID(req.params.id) }, updateDoc, function (err, doc) {
+  db.collection(SONGS_COLLECTION).updateOne({ _id: new ObjectID(req.params.id) }, updateDoc, function (err, doc) {
     if (err) {
-      handleError(res, err.message, "Failed to update contact.");
+      handleError(res, err.message, "Failed to update song.");
     } else {
       res.status(204).json(doc);
     }
   });
 });
 
-app.delete("/contacts/:id", function(req, res) {
-  db.collection(CONTACTS_COLLECTION).deleteOne({ _id: new ObjectID(req.params.id) }, function (err, result) {
+app.delete("/songs/:id", function(req, res) {
+  db.collection(SONGS_COLLECTION).deleteOne({ _id: new ObjectID(req.params.id) }, function (err, result) {
     if (err) {
-      handleError(res, err.message, "Failed to delete contact.");
+      handleError(res, err.message, "Failed to delete song.");
     } else {
       res.status(204).end();
+    }
+  });
+});
+
+/* "/songs/:id/file"
+ * GET: download song file by id
+ */
+
+app.get("/songs/:id/file", function(req, res) {
+  db.collection(SONGS_COLLECTION).findOne({ _id: new ObjectID(req.params.id) }, function (err, doc) {
+    if (err) {
+      handleError(res, err.message, "Failed to get song.");
+    } else {
+      var bucket = new mongodb.GridFSBucket(db);
+      res.status(200);
+      bucket.openDownloadStream(doc.file_id).pipe(res);
     }
   });
 });
